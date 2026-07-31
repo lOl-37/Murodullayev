cb713f60c7c2ede73ed2e61a652b588ff59ffaf5
lOl-37 <begzodmurodullayev209@gmail.com>
2026-07-31 10:12:06 +0500

    Add SQLAlchemy models for database tables

---
+++ database.py
@@ -0,0 +1,48 @@
+from sqlalchemy import create_engine, Column, Integer, String, Text
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+SQLACHEMY_DATABASE_URL = "sqlite:///./darslar.db"
+
+engine = create_engine(
+    SQLACHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
+
+class DarsModel(Base):
+    __tablename__ = "darslar"
+    
+    id = Column(Integer, primary_key=True, index=True)
+    nomi = Column(String, nullable=False)
+    daraja = Column(String, nullable=False)
+    tavsif = Column(Text, nullable=False)
+    video_url = Column(String, nullable=True)
+    kodlar = Column(Text, nullable=True)
+
+class UserModel(Base):
+    __tablename__ = "users"
+    
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String, unique=True, index=True)
+    password = Column(String)
+
+# 1. Darslarga izohlar uchun jadval
+class IzohModel(Base):
+    __tablename__ = "izohlar"
+    
+    id = Column(Integer, primary_key=True, index=True)
+    dars_id = Column(Integer, index=True)
+    username = Column(String)
+    matn = Column(Text)
+
+# 2. Adminga yuborilgan shaxsiy savollar uchun jadval
+class SavolModel(Base):
+    __tablename__ = "savollar"
+    
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String)
+    matn = Column(Text)
+
+Base.metadata.create_all(bind=engine)
